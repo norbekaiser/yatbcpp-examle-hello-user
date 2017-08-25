c072ba7aa35ee4b9afb402e333890118663dbfe5
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-25 16:32:26 +0200

    [CI] internationalized example

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@
 yatbcpp::Token token("replace me with a usefull token");
 yatbcpp::Bot B(token);
 
+/**
+ * Simple callback to say hello world or if it knows the user hello [first name]
+ * @param M the Message which the callback will get
+ */
 void update_callback(yatbcpp::Message M){
-    std::string message= "Hallo " ;
+    std::string message= "Hello " ;
     if( M.getFrom() ){
         message += M.getFrom().value().getFirst_name();
     }
     else{
-        message += "Welt";
+        message += "World";
     }
     yatbcpp::sendMessage sendMessage(M.getChat(),message);
     B.send(sendMessage);
@@ -21,7 +25,7 @@ int main() {
     using namespace yatbcpp;
 
     B.addOnMessageListener(update_callback);
-//
+
     B.startUpdating();
     B.startLongPoll();
 

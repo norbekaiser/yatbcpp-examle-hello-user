b7509cd26ce37514d90b537e2b76053fa65697ee
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-28 17:57:05 +0200

    [CI] outhc, am modified for example for upcoming api state and changed deploy to repo without typo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <yatbcpp/yatbcpp.h>
 
-
 yatbcpp::Token token("replace me with a usefull token");
 yatbcpp::Bot B(token);
 
@@ -10,25 +9,36 @@ yatbcpp::Bot B(token);
  * @param M the Message which the callback will get
  */
 void update_callback(yatbcpp::Message M){
-    std::string message= "Hello " ;
+    using namespace std;
+    using namespace yatbcpp;
+    string message= "Hello " ;
+
     if( M.getFrom() ){
         message += M.getFrom().value().getFirst_name();
     }
     else{
         message += "World";
     }
-    yatbcpp::sendMessage sendMessage(M.getChat(),message);
-    B.send(sendMessage);
+    auto gm = yatbcpp::getMe();
+    auto Usr = yatbcpp::getMe::perform_request(token,gm);
+    sendMessage sendMessage(M.getChat(),message);
+    telegram_method<yatbcpp::Message>::perform_request(token,sendMessage);
+
 }
+
+
 
 int main() {
     using namespace yatbcpp;
-
+    //Getting The Bot as User from the GetMe Function
+    User U = B.getMe();
+    //Adding our onMessageListener
     B.addOnMessageListener(update_callback);
-
+    //Starting the update queue for incoming or pending updates
     B.startUpdating();
+    //Starting the poll for updates
     B.startLongPoll();
-
+    //Waiting to join the thread
     B.join();
     return 0;
 }
